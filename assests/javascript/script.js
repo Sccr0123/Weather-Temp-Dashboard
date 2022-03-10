cf090a9715f43126d3c4017e3fdf7037beadb489
Zachary R Thomas <sccr0123@gmail.com>
2022-03-10 00:21:48 -0500

    Added 5-Day, CurrentCity, and Search

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,6 +1,22 @@
 const APIKey = "0ff69f3d13585fb83dee9d3379f0e553";
 
-var getLatLong = function (city) {
+function generatePage() {
+    var cityName = "Detroit";
+    var curIconE1 = "";
+    var curTempE1 = "";
+    var curHumE1 = "";
+    var curWindSpeed = "";
+    var curUvIndE1 = "";
+    var searchHisE1 = "";
+    var curDayE1 = "";
+    var fiveDayE1 = "";
+    var history = "";
+
+    getLatLong(cityName);
+    console.log("Page Loaded");
+}
+
+function getLatLong(city) {
     fetch(
 		$(
 			`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`
@@ -14,7 +30,7 @@ var getLatLong = function (city) {
 		});
 };
 
-var getWeather = function (lat, long) {
+function getWeather(lat, long) {
 	fetch($(`api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${APIKey}`))
 		.then(function (response) {
 			return response.json();
@@ -24,5 +40,7 @@ var getWeather = function (lat, long) {
 		});
 };
 
-getLatLong("Detroit");
+$(document).ready(function () {
+    generatePage();
+});
 
