2db1efbd69d85d98973180a95ddd27e8b655354e
Zachary R Thomas <sccr0123@gmail.com>
2022-03-10 19:03:58 -0500

    Created Layout In HTML

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,7 +1,7 @@
 const APIKey = "0ff69f3d13585fb83dee9d3379f0e553";
 
 function generatePage() {
-    var cityName = "Detroit";
+	var cityName = "Detroit";
     var curIconE1 = "";
     var curTempE1 = "";
     var curHumE1 = "";
@@ -10,28 +10,29 @@ function generatePage() {
     var searchHisE1 = "";
     var curDayE1 = "";
     var fiveDayE1 = "";
-    var history = "";
+	var history = "";
 
-    getLatLong(cityName);
     console.log("Page Loaded");
+	getLatLong(cityName);
 }
 
 function getLatLong(city) {
-    fetch(
-		$(
-			`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`
-		)
-	)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`)
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (data) {
-			console.log(data);
+			var long = data[0].lon;
+			var lat = data[0].lat;
+			console.log(lat);
+			console.log(long);
+			
+			getWeather(lat, long);
 		});
 };
 
 function getWeather(lat, long) {
-	fetch($(`api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${APIKey}`))
+	fetch(`api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${APIKey}`)
 		.then(function (response) {
 			return response.json();
 		})
