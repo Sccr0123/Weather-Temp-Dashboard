7ed7bbf2743fc1a742d46cb514a92059d63bb5be
Zachary R Thomas <sccr0123@gmail.com>
2022-03-12 00:49:03 -0500

    Converted HTML Layout To JQuery Layout

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,13 +1,10 @@
 // import { APIKey } from "./key";
-
 var APIKey = "0ff69f3d13585fb83dee9d3379f0e553";
+
+var curHistory = [];
 
 // dayjs.extend(window.dayjs_plugin_utc);
 // dayjs.extend(window.dayjs_plugin_timezone);
-
-var fiveDayEl = $("#FiveDay");
-var fiveDayInnerEl = $("#FiveDayInner");
-var currentDayEl = $("#CurrentDay");
 
 function clearPage() {
 	$("#curDay").empty();
@@ -49,21 +46,70 @@ function getWeather(lat, long, city) {
 			return response.json();
 		})
 		.then(function (data) {
+			
 			printCurrent(data.current, city, data.timezone);
 			printFiveDay(data.daily);
 		});
-}
+};
+
+function loadHistory() {
+	curHistory = localStorage.getItem("history");
+};
+
+function saveHistory() {
+	var tempHist = $("#searchText").val();
+	console.log(tempHist);
+	console.log(curHistory);
+	curHistory.unshift(tempHist);
+	if (curHistory.length > 8) {
+		curHistory = curHistory.slice(0, 7);
+	};
+	localStorage.setItem("history", curHistory);
+};
 
 function printCurrent(current, city, timezone) {
-	var date = new Date(current.dt * 1000).toUTCString();
+	var curDayEl = $("#CurrentDay");
+
+	var tempIcon = current.weather[0].icon;
+	var curDayIcon = `http://openweathermap.org/img/wn/${tempIcon}@2x.png`
+
+	var date = new Date(current.dt * 1000);
 	console.log(timezone);
+
 	// var date = dayjs().tz(timezone).format("M/D/YYYY");
-	currentDayEl.addClass("border border-dark");
+	curDayEl.addClass("border border-dark");
 
-	// var curDay = $("<h2></h2>");
-	// curDay.attr("id", "curDay");
+	var curDayH2 = $("<h2>");
+	curDayH2.attr("id", "curDay");
 
+	var curDayIc = $("<img>");
+	curDayIc.attr("src", curDayIcon);
+	curDayIc.attr("alt", "Current Weather Icon");
+
+	var curDayDiv = $("<div>");
+	curDayDiv.attr("class", "p-0 justify-content-center");
+	curDayDiv.attr("id", "CurrentDayInner");
+
+	var curTemp = $("<p>");
+	curTemp.attr("id", "curTemp");
+	var curWS = $("<p>");
+	curWS.attr("id", "curWS");
+	var curHum = $("<p>");
+	curHum.attr("id", "curHum");
+	var curUV = $("<p>");
+	curUV.attr("id", "curUV");
+	
+	curDayDiv.append(curTemp);
+	curDayDiv.append(curWS);
+	curDayDiv.append(curHum);
+	curDayDiv.append(curUV);
+
+	curDayEl.append(curDayH2);
+	curDayEl.append(curDayDiv);
+
+	//
 	$("#curDay").html(`${city} (${date})`);
+	$("#curDay").append(curDayIc);
 	$("#curTemp").append(`Tempature: ${parseInt(current.temp)}°`);
 	$("#curWS").append(`Wind Speed: ${current.wind_speed} mph`);
 	$("#curHum").append(`Humidity: ${current.humidity}%`);
@@ -71,24 +117,40 @@ function printCurrent(current, city, timezone) {
 }
 
 function printFiveDay(daily) {
+	var fiveDayEl = $("#FiveDay");
 	var days = [daily[1], daily[2], daily[3], daily[4], daily[5]];
 
 	fiveDayEl.addClass("border border-dark");
 
+	var fiveDayH3 = $("<h3>");
+	var fiveDayDiv = $("<div>");
+
+	fiveDayH3.text("5-Day Forcast:");
+
+	fiveDayDiv.attr("id", "FiveDayInner");
+	fiveDayDiv.attr("class", "row justify-content-around");
+
 	for (var i = 0; i < days.length; i++) {
+		var date = new Date(days[i].dt * 1000);
+		console.log(date);
 		var dayEl = $("<div>");
-		dayEl.addClass("col-2 border border-dark");
+		var tempIcon = daily[i].weather[0].icon;
+		var fiveDayIcon = `http://openweathermap.org/img/wn/${tempIcon}@2x.png`;
 
-		dayDate = document.createElement("h2");
-		dayIcon = document.createElement("img");
-		dayTemp = document.createElement("p");
-		dayWind = document.createElement("p");
-		dayHum = document.createElement("p");
+		dayEl.addClass("col-2 border border-dark bg-secondary text-light");
 
-		dayDate.textContent = "Placeholder";
-		dayTemp.textContent = `Tempature: ${daily[i].temp.max}°`;
-		dayWind.textContent = `Wind Speed: ${daily[i].wind_speed} mph`;
-		dayHum.textContent = `Humidity: ${daily[i].humidity}`;
+		dayDate = $("<h2>");
+		dayIcon = $("<img>");
+		dayTemp = $("<p>");
+		dayWind = $("<p>");
+		dayHum = $("<p>");
+
+		dayDate.text("Placeholder");
+		dayIcon.attr("src", fiveDayIcon);
+		dayIcon.attr("alt", "Weather Icon Placeholder");
+		dayTemp.text(`Tempature: ${daily[i].temp.max}°`);
+		dayWind.text(`Wind Speed: ${daily[i].wind_speed} mph`);
+		dayHum.text(`Humidity: ${daily[i].humidity}`);
 
 		dayEl.append(dayDate);
 		dayEl.append(dayIcon);
@@ -96,13 +158,18 @@ function printFiveDay(daily) {
 		dayEl.append(dayWind);
 		dayEl.append(dayHum);
 
-		fiveDayInnerEl.append(dayEl);
+		fiveDayDiv.append(dayEl);
 	}
+
+	fiveDayEl.append(fiveDayH3);
+	fiveDayEl.append(fiveDayDiv);
 }
 
 $(document).ready(function () {
+	loadHistory();
 	$("#searchBtn").on("click", function () {
-		clearPage();
+		//clearPage();
+		saveHistory();
 		var cityName = $("#searchText").val();
 		generatePage(cityName);
 	});
